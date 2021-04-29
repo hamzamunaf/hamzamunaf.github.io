35a6ed978ac5345b8b16a0fd3c9465e9f8b10c95
Hamza Munaf <46735474+hamzamunaf@users.noreply.github.com>
2021-04-28 23:14:55 -0700

    Updates

---
+++ precache-manifest.90d077e18f2af00e299b691c636e3784.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "76be27e1281d3f2b2fd3122d5bb5188a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "986842f26c20a0868be4",
+    "url": "/static/css/3.3a26ebde.chunk.css"
+  },
+  {
+    "revision": "a7eb0d62b4fc35a6b751",
+    "url": "/static/css/4.ee2a0142.chunk.css"
+  },
+  {
+    "revision": "c883a56dc0230226bd7d",
+    "url": "/static/css/main.252c7f45.chunk.css"
+  },
+  {
+    "revision": "fb33feb151d7e8843e56",
+    "url": "/static/js/2.52dc0ab3.chunk.js"
+  },
+  {
+    "revision": "986842f26c20a0868be4",
+    "url": "/static/js/3.3467f63f.chunk.js"
+  },
+  {
+    "revision": "a7eb0d62b4fc35a6b751",
+    "url": "/static/js/4.82f5ce68.chunk.js"
+  },
+  {
+    "revision": "c883a56dc0230226bd7d",
+    "url": "/static/js/main.7c6a7509.chunk.js"
+  },
+  {
+    "revision": "c2d8bfb12d503823d4d2",
+    "url": "/static/js/runtime-main.f3f2a290.js"
+  },
+  {
+    "revision": "21f233e19402cc4a66866a7f31191f0d",
+    "url": "/static/media/Agustina.21f233e1.woff"
+  },
+  {
+    "revision": "db1a0155e6ec00dad9ce432d3a107076",
+    "url": "/static/media/CraftCenterImage.db1a0155.JPG"
+  },
+  {
+    "revision": "ee6539921d713482b8ccd4d0d23961bb",
+    "url": "/static/media/Montserrat-Regular.ee653992.ttf"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/static/media/logo.25bf045c.svg"
+  },
+  {
+    "revision": "39eff4a91b01e00a32cb78f3797eec88",
+    "url": "/static/media/talksCardBack.39eff4a9.svg"
+  }
+]);
