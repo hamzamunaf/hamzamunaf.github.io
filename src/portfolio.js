ee34365584b4ab276b055b17e65fa619d7444b34
Hamza Munaf <46735474+hamzamunaf@users.noreply.github.com>
2024-04-03 09:38:41 -0700

    Update portfolio.js

--- src/portfolio.js
+++ src/portfolio.js
@@ -270,7 +270,7 @@ const podcastSection = {
 const contactInfo = {
   title: emoji("Contact Me ☎️"),
   subtitle: "Discuss a project or just want to say hi? My Inbox is open for all.",
-  number: "+1-541-908-9529",
+  number: "",
   email_address: "Hamzamunaf.11@gmail.com"
 };
 
